7fb78eb80a7e858c2a850ae547152c756dc77a32
mchalain <marc.chalain@gmail.com>
2024-08-22 18:54:27 +0200

    sv4l2: disable dead code

--- src/sv4l2.c
+++ src/sv4l2.c
@@ -1605,7 +1605,8 @@ uint32_t sv4l2_subdev_getpixformat(int ctrlfd, int (*busformat)(void *arg, struc
 	return 0;
 }
 
-static uint32_t _set_config(void *arg, struct v4l2_subdev_format *ffs)
+#if 0
+static uint32_t _v4l2_subdev_set_config(void *arg, struct v4l2_subdev_format *ffs)
 {
 	CameraConfig_t *config = arg;
 	uint32_t fourcc = 0xFFFFFFFF;
@@ -1626,6 +1627,7 @@ static uint32_t _set_config(void *arg, struct v4l2_subdev_format *ffs)
 	config->parent.height = ffs->format.height;
 	return fourcc;
 }
+#endif
 
 int sv4l2_subdev_open(const char *subdevice)
 {
