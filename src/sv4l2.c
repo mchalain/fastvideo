e6e56c6b553301d14d4d661a2b0fbbad4b392e6d
mchalain <marc.chalain@gmail.com>
2024-12-10 16:06:56 +0100

    sv4l2: set config definition for the other devices

--- src/sv4l2.c
+++ src/sv4l2.c
@@ -1288,7 +1288,12 @@ V4L2_t *sv4l2_create2(int fd, const char *devicename, device_type_e dtype, Camer
 	}
 	dbg("V4l2 settings: %dx%d, %.4s", dev->width, dev->height, (char*)&dev->fourcc);
 	if (config)
+	{
 		config->parent.dev = dev;
+		config->parent.width = width;
+		config->parent.height = height;
+		config->parent.fourcc = fourcc;
+	}
 	return dev;
 }
 
