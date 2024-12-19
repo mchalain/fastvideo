7d840470590bf71f8ea4a52b24b0cb1709b0c000
mchalain <marc.chalain@gmail.com>
2024-12-19 15:39:16 +0100

    utils: rpi5cam_setup allows to setup the rp1_cfe on direct channel

--- utils/rpi5cam-setup.sh
+++ utils/rpi5cam-setup.sh
@@ -22,6 +22,7 @@ if [ $? -eq 0 ]; then
 	echo "select another media"
 	exit
 fi
+echo "Media : $CAMMEDIA"
 
 CSIENTITY=1
 CSINAME=$(media-ctl -d $CAMMEDIA -p | grep -e "entity $CSIENTITY:" | sed 's/- entity .*: \(.*\) (.*)/\1/')
@@ -29,7 +30,8 @@ SENSORENTITY=16
 SENSORNAME=$(media-ctl -d $CAMMEDIA -p | grep -e "entity $SENSORENTITY:" | sed 's/- entity .*: \(.*\) (.*)/\1/')
 FEENTITY=10
 FENAME=$(media-ctl -d $CAMMEDIA -p | grep -e "entity $FEENTITY:" | sed 's/- entity .*: \(.*\) (.*)/\1/')
-CHENTITY=19
+CHNAME=rp1-cfe-csi2_ch0
+CHENTITY=$(media-ctl -d $CAMMEDIA -p | grep -e "- entity .*: $CHNAME" | sed 's/- entity \(.*\): .* (.*)/\1/')
 IMGNAME=rp1-cfe-fe_image0
 IMGENTITY=$(media-ctl -d $CAMMEDIA -p | grep -e "- entity .*: $IMGNAME" | sed 's/- entity \(.*\): .* (.*)/\1/')
 CONFNAME=rp1-cfe-fe_config
@@ -41,16 +43,18 @@ SENSOR=$(media-ctl -d $CAMMEDIA -e "$SENSORNAME")
 SENSORPADIMG=0
 SENSORSTREAM=0
 SENSORPADCONF=1
-CSI=/$(media-ctl -d $CAMMEDIA -e "$CSINAME")
+CSI=$(media-ctl -d $CAMMEDIA -e "$CSINAME")
 CSIPADSENSOR=0
 CSIPADCH=4
 CSIPADCONF=1
-FE=/$(media-ctl -d $CAMMEDIA -e "$FENAME")
+FE=$(media-ctl -d $CAMMEDIA -e "$FENAME")
 FEPADIN=0
 FEPADCONF=1
 FEPADOUT=2
 FEPADSTATS=4
 IMAGE=$(media-ctl -d $CAMMEDIA -e "$IMGNAME")
+CONFIG=$(media-ctl -d $CAMMEDIA -e "$CONFNAME")
+CHANNEL=$(media-ctl -d $CAMMEDIA -e "$CHNAME")
 
 media-ctl -d $CAMMEDIA -r
 # retrieve sensor informations
@@ -69,13 +73,6 @@ fi
 
 # set the link between SENSOR and CSI entity
 media-ctl -d $CAMMEDIA --set-v4l2 "$CSIENTITY:$CSIPADSENSOR[fmt:$fmt_input/$framesize field:none colorspace:raw]"
-media-ctl -d $CAMMEDIA --link "$SENSORENTITY:$SENSORPADIMG->$CSIENTITY:$CSIPADSENSOR[1]"
-
-# set the link between SENSOR and CSI for the configuration if it exists
-media-ctl -d $CAMMEDIA --get-v4l2 $SENSORENTITY:$SENSORPADCONF | grep "not found" > /dev/null
-if [ $? -ne 0 ]; then
-  media-ctl -d $CAMMEDIA --link "$SENSORENTITY:$SENSORPADCONF->$CSIENTITY:$CSIPADCONF[1]"
-fi
 
 # the output format must be Bayer 16bits for PiSP backend
 list_fmt_output=$(v4l2-ctl -d $FE --list-subdev-mbus-codes $FEPADOUT | grep -E '0x.*[0-9,a-f]')
@@ -83,33 +80,70 @@ fmt_output=$(v4l2-ctl -d $FE --get-subdev-fmt $FEPADOUT | grep Mediabus | sed 's
 read -p "current fmt $fmt_output. Change it (y/N): " CHOICE
 if [ "$CHOICE" = y ]; then
   echo $list_fmt_output
+  read -p "set new fmt: " fmt_output
 fi
-fmt_output=SBGGR16_1X16
-CSIOUT=$FEENTITY
+OUTCSI=$FEENTITY
+OUTIMAGE=$IMAGE
 if [ "$fmt_input" = "$fmt_output" ]; then
-CSIOUT=$CHENTITY
+OUTCSI=$CHENTITY
+OUTIMAGE=$CHANNEL
 fi
 echo "fmt image "$fmt_output
 media-ctl -d $CAMMEDIA --set-v4l2 "$CSIENTITY:$CSIPADCH[fmt:$fmt_output/$framesize field:none colorspace:raw]"
-media-ctl -d $CAMMEDIA --set-v4l2 "$CSIOUT:0[fmt:$fmt_output/$framesize field:none colorspace:raw]"
-media-ctl -d $CAMMEDIA --link "$CSIENTITY:$CSIPADCH->$CSIOUT:0[1]"
+media-ctl -d $CAMMEDIA --set-v4l2 "$OUTCSI:0[fmt:$fmt_output/$framesize field:none colorspace:raw]"
 
-if [ "$CSIOUT" = "$FEENTITY" ]; then
+if [ "$OUTCSI" = "$FEENTITY" ]; then
  # set the bayer encoder
  media-ctl -d $CAMMEDIA --set-v4l2 "$FEENTITY:$FEPADOUT[fmt:$fmt_output/$framesize field:none]"
+fi
 
+media-ctl -d $CAMMEDIA --link "$CSIENTITY:$CSIPADCH->$OUTCSI:0[1]"
+
+if [ "$OUTCSI" = "$FEENTITY" ]; then
  # set the image output
  media-ctl -d $CAMMEDIA --link "$FEENTITY:$FEPADOUT->$IMGENTITY:0[1]"
- # set the configuration device
+ # set the configuration device. this must be enabled otherwise Kernel Panic :-(
  media-ctl -d $CAMMEDIA --link "$CONFENTITY:0->$FEENTITY:$FEPADCONF[1]"
 
- # set the stats device
- media-ctl -d $CAMMEDIA --link "$FEENTITY:$FEPADSTATS->$STATSENTITY:0[1]"
+ # set the stats device but disable to no be forced to stream with data
+ media-ctl -d $CAMMEDIA --link "$FEENTITY:$FEPADSTATS->$STATSENTITY:0[0]"
+fi
+
+media-ctl -d $CAMMEDIA --link "$SENSORENTITY:$SENSORPADIMG->$CSIENTITY:$CSIPADSENSOR[1]"
+# set the link between SENSOR and CSI for the configuration if it exists
+media-ctl -d $CAMMEDIA --get-v4l2 $SENSORENTITY:$SENSORPADCONF | grep "not found" > /dev/null
+if [ $? -ne 0 ]; then
+  media-ctl -d $CAMMEDIA --link "$SENSORENTITY:$SENSORPADCONF->$CSIENTITY:$CSIPADCONF[1]"
+fi
+fmt_code=$(v4l2-ctl -d $CSI --get-subdev-fmt $CSIPADCH | grep Mediabus | sed 's/[^ ].*Mediabus Code.*[ ]: \(0x.*[0-9,a-f]\) (MEDIA_BUS_FMT_\(.*\))/\1/')
+FOURCC=$(v4l2-ctl -d $OUTIMAGE --list-formats $fmt_code | grep "\[0\]" | sed "s/.*\[0\]: '\(.*\)' .*/\1/")
+echo "subdev format code $fmt_code $fmt_output => output fourcc $FOURCC"
+read -p "Change fourcc ? [y/N]" CHOICE
+if [ "$CHOICE" = "y" ]; then
+  I=0
+  for fourcc in $(v4l2-ctl -d $OUTIMAGE --list-formats $fmt_code | grep -e "\[.*[0-9]\]" | sed "s/.*\[.*[0-9]\]: '\(.*\)' .*/\1/")
+  do
+     echo "$I : $fourcc"
+     I=$(($I+1))
+  done
+  read -p "enter your choice: " CHOICE
+  if [ -n $CHOICE ]; then
+    FOURCC=$(v4l2-ctl -d $OUTIMAGE --list-formats $fmt_code | grep "\[$CHOICE\]" | sed "s/.*\[$CHOICE\]: '\(.*\)' .*/\1/")
+  fi
+  echo "subdev format code $fmt_code $fmt_output => output fourcc $FOURCC"
 fi
 
 WIDTH=$(echo $framesize | sed 's/x[0-9].*//')
 HEIGHT=$(echo $framesize | sed 's/[0-9].*x//')
-v4l2-ctl -d $IMAGE -v width=$WIDTH,height=$HEIGHT,pixelformat=$FOURCC
+v4l2-ctl -d $OUTIMAGE -v width=$WIDTH,height=$HEIGHT,pixelformat=$FOURCC
+while [ $? -ne 0 ]; do
+  echo "FourCC $FOURCC for $OUTIMAGE is not available"
+  read -p "enter another value: " FOURCC
+  if [ -z "$FOURCC" ]; then
+    break
+  fi
+  v4l2-ctl -d $OUTIMAGE -v width=$WIDTH,height=$HEIGHT,pixelformat=$FOURCC
+done
 
 echo "0: media topology"
 echo "1: Image device configuration"
@@ -124,14 +158,10 @@ case "$CHOICE" in
     v4l2-ctl -d $IMAGE -D
     ;;
   2)
-    v4l2-ctl --verbose -d $IMAGE --stream-mmap=4 --stream-skip=3 --stream-count=2 --stream-to=tempo.bayer --stream-poll
+    v4l2-ctl --verbose -d $OUTIMAGE --stream-mmap=4 --stream-skip=3 --stream-count=2 --stream-to=tempo.bayer --stream-poll
     ;;
   3)
-    read -p "set pixel format FOURCC (default: $FOURCC): " FOURCC2
-    if [ -n "$FOURCC2" ]; then
-      FOURCC=$FOURCC2
-    fi
-    v4l2-ctl --verbose -d $IMAGE --set-fmt-video=width=1456,height=1088,pixelformat=$FOURCC --stream-mmap --stream-count=1 --stream-to=test.raw
+    v4l2-ctl --verbose -d $OUTIMAGE --stream-mmap --stream-count=1 --stream-to=test.raw
     ;;
 esac
 echo Device set to $IMAGE
