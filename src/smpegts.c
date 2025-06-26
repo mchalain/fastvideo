68148832015aa60a7a76f8d9fa5899ab23aad95d
mchalain <marc.chalain@gmail.com>
2025-06-26 09:04:04 +0200

    smpegts: crc32 algo

--- src/smpegts.c
+++ src/smpegts.c
@@ -150,6 +150,72 @@ DeviceConf_t *mpegts_createconfig(void)
 
 static int dumpfd = 0;
 
+#define CRC_32_RESULT_WIDTH                 32u
+#define CRC_32_POLYNOMIAL                   0x04C11DB7u
+#define CRC_32_INIT_VALUE                   0xFFFFFFFFu
+#define CRC_32_XOR_VALUE                    0xFFFFFFFFu
+
+static uint8_t reverse_8bits(uint8_t value)
+{
+	value = (value & 0xF0) >> 4u | (value & 0x0F) << 4u;
+	value = (value & 0xCC) >> 2u | (value & 0x33) << 2u;
+	value = (value & 0xAA) >> 1u | (value & 0x55) << 1u;
+
+	return value;
+}
+
+static uint32_t reverse_32bits(uint32_t value)
+{
+	uint32_t reversed = 0u;
+
+	for(uint8_t i = 31u; value; )
+	{
+		reversed |= (value & 1u) << i;
+		value >>= 1u;
+		-- i;
+	}
+
+	return reversed;
+}
+
+static uint32_t crc32(const uint8_t *buffer, uint16_t length)
+{
+	uint32_t retVal = 0u;
+
+	if (buffer != NULL)
+	{
+		retVal = CRC_32_INIT_VALUE;
+
+		/* Do calculation procedure for each byte */
+		for (int i = 0; i < length; i++)
+		{
+			/* XOR new byte with temp result */
+			retVal ^= (reverse_8bits(buffer[i]) << (CRC_32_RESULT_WIDTH - 8u));
+
+			/* Do calculation for current data */
+			for (int j = 0; j < 8; j++)
+			{
+				if (retVal & (1u << (CRC_32_RESULT_WIDTH - 1u)))
+				{
+					retVal = (retVal << 1u) ^ CRC_32_POLYNOMIAL;
+				}
+				else
+				{
+					retVal = (retVal << 1u);
+				}
+			}
+		}
+
+		/* XOR result with specified value */
+		retVal ^= CRC_32_XOR_VALUE;
+	}
+
+	/* Reflect result */
+	retVal = reverse_32bits(retVal);
+
+	return retVal;
+}
+
 int _client_filldata(Dev_t *dev, size_t mtu)
 {
 	int ret = 1;
