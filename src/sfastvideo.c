b54dbe933c4ab22dfee0f4db85c20aaab36b9b88
mchalain <marc.chalain@gmail.com>
2025-10-23 08:36:43 +0200

    libfastvideo: new list management functions

--- src/sfastvideo.c
+++ src/sfastvideo.c
@@ -46,24 +46,111 @@ FastVideoList_t *fastvideolist_insert(FastVideoList_t *list, void *device)
 	return list;
 }
 
+FastVideoList_t *fastvideolist_push(FastVideoList_t *list, FastVideoList_t *newentry)
+{
+	FastVideoList_t *entry = NULL;
+	for (entry = list; entry != NULL; entry = entry->next)
+	{
+		if (entry->entity == newentry->entity)
+			return list;
+	}
+	if (list)
+	{
+		newentry->last = list->last;
+		list->previous = newentry;
+	}
+	newentry->next = list;
+	return newentry;
+}
+
+FastVideoList_t *fastvideolist_poplast(FastVideoList_t *list, FastVideoList_t **entry)
+{
+	if (!list)
+		return NULL;
+	list->iterator = list->last;
+	return fastvideolist_pop(list, entry);
+}
+
+FastVideoList_t *fastvideolist_pop(FastVideoList_t *list, FastVideoList_t **entry)
+{
+	if (list == NULL)
+		return NULL;
+	FastVideoList_t *current = list->iterator;
+	if (current == NULL)
+		current = list;
+	if (entry)
+		*entry = current;
+	if (list->iterator->previous)
+		list->iterator->previous->next = current->next;
+	if (list->iterator->next)
+		list->iterator->next->previous = current->previous;
+	list->iterator = list->iterator->next;
+	if (current == list->last)
+		list->last = current->previous;
+	if (entry)
+		(*entry)->previous = (*entry)->next = NULL;
+	if (current == list)
+		list = NULL;
+	return list;
+}
+
 void fastvideolist_reset(FastVideoList_t *list)
 {
-	list->iterator = NULL;
+	if (list)
+		list->iterator = NULL;
 }
 
 FastVideoList_t *fastvideolist_last(FastVideoList_t *list)
 {
+	if (!list)
+		return NULL;
 	return list->last;
 }
 
 FastVideoList_t *fastvideolist_first(FastVideoList_t *list)
 {
-	list->iterator = NULL;
+	if (list)
+		list->iterator = NULL;
 	return list;
+}
+
+FastVideoList_t *fastvideolist_current(FastVideoList_t *list)
+{
+	if (list)
+		return list->iterator;
+	return NULL;
+}
+
+FastVideoList_t *fastvideolist_up(FastVideoList_t *list)
+{
+	if (list)
+	{
+		if (list->iterator)
+			list->iterator = list->iterator->next;
+		else
+			list->iterator = list;
+		return list->iterator;
+	}
+	return NULL;
+}
+
+FastVideoList_t *fastvideolist_down(FastVideoList_t *list)
+{
+	if (list)
+	{
+		if (list->iterator)
+			list->iterator = list->iterator->previous;
+		else
+			list->iterator = list->last;
+		return list->iterator;
+	}
+	return NULL;
 }
 
 int fastvideolist_islast(FastVideoList_t *list, void *entity)
 {
+	if (!list)
+		return 0;
 	if (list->last->entity == entity)
 		return 1;
 	return 0;
@@ -71,6 +158,8 @@ int fastvideolist_islast(FastVideoList_t *list, void *entity)
 
 int fastvideolist_isfirst(FastVideoList_t *list, void *entity)
 {
+	if (!list)
+		return 0;
 	if (list->entity == entity)
 		return 1;
 	return 0;
