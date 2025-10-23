0a4d1c0ed70fc21d386218ad9e5c703bc6ffaccd
mchalain <marc.chalain@gmail.com>
2025-10-23 11:43:02 +0200

    utils: new unix socket client

---
+++ utils/unixclient.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <fcntl.h>
+
+#include "log.h"
+#include "unixsocket.h"
+
+typedef struct app_s app_t;
+struct app_s
+{
+	client_t *client;
+	int fd;
+};
+
+int _client_receive(void *data, client_t *clt, const char *buffer, size_t length)
+{
+	app_t *app = (app_t *)data;
+	//dbg("recieve %lu bytes", length);
+	if (app->fd > 0)
+		write(app->fd, buffer, length);
+}
+
+int main_run(app_t *app)
+{
+	while (1)
+	{
+		client_wait(app->client, 0, NULL);
+	}
+}
+
+int main(int argc, char * const argv[])
+{
+	app_t app = {0};
+	const char *serverpath = "/tmp/fastsetting_socket";
+	unsigned int mode = 0;
+	const char *logfile = NULL;
+
+	int opt;
+	do
+	{
+		opt = getopt(argc, argv, "L:f:");
+		switch (opt)
+		{
+			case 'L':
+				dbg("coucou");
+				logfile = optarg;
+			break;
+			case 'f':
+				serverpath = optarg;
+			break;
+		}
+	} while(opt != -1);
+
+	if (logfile)
+	{
+		app.fd = open(logfile, O_WRONLY | O_CREAT);
+		warn("dump to file %s (%d %m)", logfile, app.fd);
+	}
+	app.client = client_create(serverpath);
+	client_attach_receive(app.client, _client_receive, &app);
+	main_run(&app);
+	client_destroy(app.client);
+	if (app.fd > 0)
+		close(app.fd);
+	return 0;
+}
