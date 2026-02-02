78fbc613287ea2f5293f3b3c6701287ad171c858
mchalain <marc.chalain@scantech.com>
2026-02-02 14:07:13 +0100

    data: add ShaderToy bayer converter

--- data/ShaderToy/Bayer.glsl
+++ data/ShaderToy/Bayer.glsl
@@ -1,10 +1,14 @@
-#define texture texture2D
-#define iChannel0 vTexture
 vec3 coef = vec3(1.0, 0.5, 1.0);
+const vec4 bayer = vec4(0.0, 1.0, 2.0, 3.0);
 
 vec3 pixel(in vec2 uv)
 {
-    return texture(iChannel0, uv).rgb;
+	return texture(iChannel0, uv).rgb;
+}
+
+int mymod(in int A, in int B)
+{
+	return A - (A / B) * B;
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
@@ -12,7 +16,9 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 	vec3 col = vec3(0.0);
 	vec2 uv = fragCoord/iResolution.xy;
 	vec2 scale = vec2(1.0) / iResolution.xy;
-	vec2 posin = mod(uv, vec2(2.0, 2.0));
+	int iCoordX = int(fragCoord.x);
+	int iCoordY = int(fragCoord.y);
+	int pos = mymod(iCoordX, 2) * 2 + mymod(iCoordY, 2);
 
 	vec3 p0 = pixel(uv);
 	vec3 p1;
@@ -30,13 +36,14 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 		p3 = pixel(uv + scale * vec2(1.0,1.0));
 	else
 		p3 = pixel(uv + scale * vec2(-1.0,-1.0));
-	if (posin.x < 1.0 && posin.y < 1.0)
-		col = vec3(p0.r * coef.r, p1.r * coef.g + p2.r * coef.g, p3.r * coef.b);
-	if (posin.x > 1.0 && posin.y < 1.0)
-		col = vec3(p1.r * coef.r, p0.r * coef.g + p3.r * coef.g, p2.r * coef.b);
-	if (posin.x < 1.0 && posin.y > 1.0)
-		col = vec3(p1.r * coef.r, p0.r * coef.g + p3.r * coef.g, p1.r * coef.b);
-	if (posin.x > 1.0 && posin.y > 1.0)
-		col = vec3(p3.r * coef.r, p1.r * coef.g + p2.r * coef.g, p0.r * coef.b);
+
+	if (pos == int(bayer.b))
+			col = vec3(p3.r * coef.r, (p1.r + p2.r) * coef.g, p0.r * coef.b);
+	if (pos == int(bayer.g))
+			col = vec3(p1.r * coef.r, (p0.r + p3.r) * coef.g, p2.r * coef.b);
+	if (pos == int(bayer.a))
+			col = vec3(p2.r * coef.r, (p0.r + p3.r) * coef.g, p1.r * coef.b);
+	if (pos == int(bayer.r))
+			col = vec3(p0.r * coef.r, (p1.r + p2.r) * coef.g, p3.r * coef.b);
 	fragColor = vec4(col,1.0);
 }
--- data/ShaderToy/vertex.glsl
+++ data/ShaderToy/vertex.glsl
@@ -8,7 +8,7 @@ varying vec2 iMouse;
 
 void main (void)
 {
-	fragCoord = (vec2(0.5, 0.5) + vPosition.xy / 2.0) * vResolution.xy;
+	fragCoord = (vec2(0.5, 0.5) - vPosition.xy / 2.0) * vResolution.xy;
 	iResolution = vResolution.xy;
 	iMouse = iResolution / 2.0;
 	gl_Position = vec4(vPosition,1.);
