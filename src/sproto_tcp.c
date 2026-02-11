ed1698ea4594b935cd0b44ddc7489efb9acf63ed
Marc Chalain <marc.chalain@gmail.com>
2026-02-11 17:59:33 +0100

    sproto: remove SIGPIPE at disconnection and append message

--- src/sproto_tcp.c
+++ src/sproto_tcp.c
@@ -273,19 +273,22 @@ static ssize_t proto_send(void *arg, const void *buf, size_t len, Proto_Flags_t 
 		proto_connect_server(arg);
 	else if (proto->clientfd == -1)
 	{
-		warn("no client connected");
+		warn("tcp: no client connected");
 		return -1;
 	}
 	/// server mode and no client are connected
 	if (proto->clientfd == -1)
 		return len;
 	if (len == 0)
-		warn("send empty packet");
+		warn("tcp: send empty packet");
+	int sflags = MSG_NOSIGNAL;
+	if (flags & Proto_More)
+		sflags = MSG_MORE;
 	while (ret == -1 && errno == EAGAIN)
-		ret = send(proto->clientfd, buf, len, 0);
+		ret = send(proto->clientfd, buf, len, sflags);
 	if (ret < 0)
 	{
-		err("mpegts: sending on tcp error %m");
+		err("tcp: sending error %m");
 		close(proto->clientfd);
 		proto->clientfd = -1;
 		if (proto->serverfd > 0)
@@ -303,13 +306,14 @@ static ssize_t proto_recv(void *arg, void *buf, size_t len, Proto_Flags_t flags)
 	ssize_t ret = -1;
 	if (proto->clientfd == -1)
 	{
-		warn("no client connected");
+		warn("tcp: no client connected");
 		return -1;
 	}
-	ret = recv(proto->clientfd, buf, len, 0);
+	int sflags = MSG_NOSIGNAL;
+	ret = recv(proto->clientfd, buf, len, sflags);
 	if (ret < 0 && errno != EAGAIN)
 	{
-		err("mpegts: receiving on tcp error %m");
+		err("tcp: receiving error %m");
 		close(proto->clientfd);
 		proto->clientfd = -1;
 		if (proto->serverfd > 0)
